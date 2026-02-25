902ebeccd91cf8724133ddb4ca587f603040b088
niju646 <nijusajeevnj@gmail.com>
2026-02-25 12:48:29 +0530

    updated the updateRouteById

--- controllers/tracker/routeController.js
+++ controllers/tracker/routeController.js
@@ -25,24 +25,48 @@ const getRouteById = async (req, res) => {
 const updateRouteById = async (req, res) => {
   try {
     const { id } = req.params;
-    const { route_name, type } = req.body;
-    const studentroute = await StudentRoutes.findOne({
+    const { start, stop, route_no, type, vehicle_id, driver_id, isLock } = req.body;
+    const pickupRoute = await StudentRoutes.findOne({
       where: {
         id: id,
       },
     });
-    if (!studentroute) {
+    if (!pickupRoute) {
       return res.status(404).json({ message: "No route found" });
     }
 
-    await studentroute.update({
-      route_name: route_name ?? studentroute.route_name,
-      type: type ?? studentroute.type,
+    const pickupRouteName = route_no
+      ? `${start}-${stop}-${route_no}`
+      : `${start}-${stop}`;
+
+    const dropRouteName = route_no
+      ? `${stop}-${start}-${route_no}`
+      : `${stop}-${start}`;
+
+    await pickupRoute.update({
+      route_name: pickupRouteName ?? pickupRoute.route_name,
+      vehicle_id: vehicle_id ?? pickupRoute.vehicle_id,
+      driver_id: driver_id ?? pickupRoute.driver_id,
+      type: type ?? pickupRoute.type,
+      isLock: isLock ?? pickupRoute.isLock,
     });
+
+    const dropRoute = await StudentRoutes.findOne({
+      where: { pickId: pickupRoute.id }
+    });
+
+    if (dropRoute) {
+      await dropRoute.update({
+        route_name: dropRouteName,
+        vehicle_id: vehicle_id ?? dropRoute.vehicle_id,
+        driver_id: driver_id ?? dropRoute.driver_id,
+        isLock: isLock ?? dropRoute.isLock,
+      });
+    }
 
     return res.status(200).json({
       message: "Stop updated successfully",
-      data: studentroute,
+      data: pickupRouteName, dropRouteName
     });
   } catch (error) {
     console.error("Error updating route:", error);
