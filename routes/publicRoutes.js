dcc9cb24a387f22f111d7924edbef3a5808c242d
ishaque risan <riswanp747970@gmail.com>
2026-04-02 17:41:56 +0530

    set ratelimit in login

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,8 +1,17 @@
 const publicController = require("../controllers/publicController");
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const router = express.Router();
 
+const loginRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 5, // Limit each IP to 5 login requests per `window` (here, per 15 minutes)
+  message: "Too many login attempts from this IP, please try again after 15 minutes",
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 // Public Routes
-router.post("/login", publicController.login);
+router.post("/login", loginRateLimiter, publicController.login);
 
 module.exports = router;
