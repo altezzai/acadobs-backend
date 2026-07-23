4bfbd32dcd297d24121545e700714533bc854555
altezzai <167423411+altezzai@users.noreply.github.com>
2026-07-23 11:55:23 +0530

    Increase login request limit to 30 per 15 minutes

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 const loginRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 5, // Limit each IP to 5 login requests per `window` (here, per 15 minutes)
+  limit: 30, // Limit each IP to 5 login requests per `window` (here, per 15 minutes)
   message: "Too many login attempts from this IP, please try again after 15 minutes",
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
